d043be5c9cd19273266ecde9cafd05dd6470b710
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2021-01-06 16:36:29 +0300

    FEAT: jest configured for typescript

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,8 @@
+import { Utils } from '../app/Utils'
+
+describe('Utils test suite', () => {
+  test('first test', () => {
+    const result = Utils.toUpperCase('abc')
+    console.log('test work')
+  })
+})
